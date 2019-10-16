a1f1e0d546da2e9425e56fd2acda78f871a94b8e
Gavin Riggi <riggi.gavin@gmail.com>
2019-10-16 06:25:37 -0400

    #50: Finished basic data access object class and imported to main

--- login/dao.js
+++ login/dao.js
@@ -4,49 +4,74 @@
  *
  **/
 
-const fs = require('fs')
-const sqlite3 = require('sqlite3')
+const fs = require('fs');
+const sqlite3 = require('sqlite3');
 
 /** DAO is the abstraction class for interactions with the database.  
  * 
  *
  *
 **/
-class DAO {
+module.exports = class DAO {
 
-    const TABLE_PRODUCTS = "CREATE TABLE IF NOT EXISTS Products(sku int NOT NULL PRIMARY KEY, description text NOT NULL UNIQUE, unit_price real NOT NULL);"
-    const TABLE_CUSTOMERS = "CREATE TABLE IF NOT EXISTS Customers(custId int NOT NULL PRIMARY KEY, firstname text NOT NULL, lastname text NOT NULL, email text UNIQUE, phone varchar(16), address text, city text, zip varchar(9), points int NOT NULL, joined text NOT NULL, lastSeen text NOT NULL);"
-    const TABLE_SALES = "CREATE TABLE IF NOT EXISTS Sales(saleId int NOT NULL PRIMARY KEY, FOREIGN KEY(customer) REFERENCES Customers(custId), soldItems blob NOT NULL, pointsUsed int NOT NULL, pointsEarned int NOT NULL, total real NOT NULL);"
-    const TABLE_USERS = "CREATE TABLE IF NOT EXISTS Users(userId int NOT NULL PRIMARY KEY, username text NOT NULL, pass blob NOT NULL, salt blob NOT NULL, group text NOT NULL);"
+    
 
     /** constructor creates the database connection by opening a database at the given path.
       * Upon successful connection, it creates the necessary tables if they do not exist. 
       * @param dbFilePath        the desired path for the database
      **/
     constructor(dbFilePath) {
+
+	const TABLE_PRODUCTS = `CREATE TABLE IF NOT EXISTS Products( 
+        sku integer NOT NULL PRIMARY KEY, 
+        description text NOT NULL UNIQUE, 
+        unit_price real NOT NULL)`;
+	const TABLE_CUSTOMERS = `CREATE TABLE IF NOT EXISTS Customers( 
+        custId integer NOT NULL PRIMARY KEY, 
+        firstname text NOT NULL, 
+        lastname text NOT NULL, 
+        email text UNIQUE, 
+        phone text, 
+        address text,  
+        city text, zip text, 
+        points integer NOT NULL, 
+        joined text NOT NULL, 
+        lastSeen text NOT NULL)`;
+	const TABLE_SALES = `CREATE TABLE IF NOT EXISTS Sales( 
+        saleId integer NOT NULL PRIMARY KEY, 
+        FOREIGN KEY(customer) REFERENCES Customers(custId), 
+        soldItems blob NOT NULL, 
+        pointsUsed integer NOT NULL, 
+        pointsEarned integer NOT NULL, 
+        total real NOT NULL)`;
+	const TABLE_USERS = `CREATE TABLE IF NOT EXISTS Users( 
+        userId integer NOT NULL PRIMARY KEY, 
+        username text NOT NULL, 
+        pass blob NOT NULL, 
+        salt blob NOT NULL, 
+        group text NOT NULL)`;
+
+
+
+
+
 	this.db = new sqlite3.Database(dbFilePath, (err) => {
 	    if (err) {
-		console.log('Database connection failed', err)
+		console.log('Database connection failed', err);
 	    } else {
-		console.log('Connected to database')
-		createTables(this)
+		console.log('Connected to database');
+		console.log('Database creation for Products, Customers, Sales, and Users');
+		console.log(TABLE_PRODUCTS);
+		this.db.run(TABLE_PRODUCTS);
+		this.db.run(TABLE_CUSTOMERS);
+		//this.db.run(TABLE_SALES);
+		//this.db.run(TABLE_USERS);
 	    }
 	})
+	
     }
 
-    /** createTables creates the schema for our datastore if they do not already exist
-      * Currently creates Products, Customers, Sales, and Users
-      * @param dao     Passes the parent object for the purposes of establishing proper scope
-      **/
-    createTables(dao) {
-	console.log("Database creation for Products, Customers, Sales, and Users")
-	dao.db.run(TABLE_PRODUCTS)
-	dao.db.run(TABLE_CUSTOMERS)
-	dao.db.run(TABLE_SALES)
-	dao.db.run(TABLE_USERS)
-
-    }
-
+    
  /** /** run is an abstraction of the existing run function.
       * 
       * @param sql               the SQL query to execute
@@ -70,4 +95,4 @@ class DAO {
 }
 
 
-module.exports = DAO    
+//module.exports.DAO = DAO;    
--- login/main.js
+++ login/main.js
@@ -1,9 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 
-const fs = require('fs')
-// .verbose() enables verbose mode which captures stack traces when enqueing queries.
-const sqlite3 = require('sqlite3').verbose();
 
+var DAO = require('./dao.js')
+ 
 var loginTest = [
 	{
 		username: "edmund",
@@ -49,110 +48,33 @@ function accountExists(username){
 //Clear login and show cashier window
 function clearLogin(id){
      
-        var box = document.getElementById(id)
-	box.style.transition = "opacity 1.0s linear 0s"
-	box.style.opacity = 0
-        setTimeout(function(){
-	
-	   
-	    box.style.display = "none"
-	    var cashier = document.getElementById("cashierwindow")
-	    cashier.style.display = "block"
-	}, 1000)
+    var box = document.getElementById(id);
+    box.style.transition = "opacity 1.0s linear 0s";
+    box.style.opacity = 0;
+    setTimeout(function(){	   
+	    box.style.display = "none";
+	    var cashier = document.getElementById("cashierwindow");
+	    cashier.style.display = "block";
+    }, 1000);
 
 }
 
 function uLogin(){
-	var username = document.getElementById("username").value
-	var password = document.getElementById("password").value
-	var exist = accountExists(username)
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var exist = accountExists(username);
 	if(exist == true){
-		console.log("Username: " + username + '\n' + "Password: " + password)
-		clearLogin("wrapper")
+	    console.log("Username: " + username + '\n' + "Password: " + password);
+	    clearLogin("wrapper");
 	}
 	else{
-		console.log("No such account")
+	    console.log("No such account");
 	}
 }
 
-app.on('ready', createWindow)
-
-/////////// SQLite code ///////////
-
-// creating a Database Object.
-// The sqlite3.Database() returns a Database object and opens the database connection automatically.
-let customerDB = new sqlite3.Database('../database/db/customer.db', function(err){
-    // if err is != null then we print out an error message.
-    if(err){
-        return console.error(err.message);
-    }
-    // print out this if we successfully connected wihtout any errors.
-	console.log('Connected to the customer database.');
-
-});
- 
-let productDB = new sqlite3.Database('../database/db/product.db', function(err){
-    // if err is == null then we print out an error message.
-    if(err){
-        return console.error(err.message);
-    }
-    // print out this if we successfully connected wihtout any errors.
-	console.log('Connected to the product database.');
-	
-		// createTable() creates table
-		productDB.exec(productDBSchema, function(err){
-			if(err){
-				return console.error(err.message);
-			}
-		});
-});
-
-// productDB Schema
-productDBSchema = `CREATE TABLE IF NOT EXISTS Products(
-	sku text NOT NULL PRIMARY KEY,
-	description text NOT NULL,
-	unit_price REAL NOT NULL
-);`
-
-// getter function to get row by sku
-// returns an array with sku, description, and unit price indexed in that order
-function findItemBySKU (sku){
-	var sql = 'SELECT sku, description, unit_price ';
-	sql += 'FROM Products ';
-	sql += 'WHERE sku = ? ';
-        
-	return productDB.get(sql, sku, function(err, row){
-	    if(err){
-		console.log('error finding item with sku ' + sku, err);
-		    return console.error(err.message);
-		}
-		console.log(row);
-		return row;
-	})
-}
+app.on('ready', createWindow);
 
 
-function addTableItem(sku) {
-    console.log("SKU is : ",findItemBySKU(sku));
-}
+const DBPATH = "./datastore.db";
+var DAOtest = new DAO(DBPATH);
 
-
-//addTableItem("123415");
-console.log(findItemBySKU("123415"))
-
-// close the database connection.
-customerDB.close(function(err){
-    if(err){
-	console.log('error closing customerDB: ', err);
-        return console.error(err.message);
-    }
-    console.log('Close the customer database connection.')
-});
-
-productDB.close(function(err){
-    if(err){
-	console.log('error closing productDB: ',err);
-        return console.error(err.message);
-    }
-    console.log('Close the product database connection.')
-});
